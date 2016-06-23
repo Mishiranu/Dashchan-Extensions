27608a546e389fdac1a3e7dbc06529cb4b62e912
Mishiranu <fukurou.mishiranu@gmail.com>
2016-06-23 19:06:10 +0300

    StringUtils call fix

--- src/com/mishiranu/dashchan/chan/kurisach/KurisachPostsParser.java
+++ src/com/mishiranu/dashchan/chan/kurisach/KurisachPostsParser.java
@@ -379,17 +379,17 @@ public class KurisachPostsParser implements GroupParser.Callback
 			}
 			case EXPECT_SUBJECT:
 			{
-				mPost.setSubject(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setSubject(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_NAME:
 			{
-				mPost.setName(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setName(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_TRIPCODE:
 			{
-				mPost.setTripcode(StringUtils.emptyIfNull(StringUtils.clearHtml(text).trim()));
+				mPost.setTripcode(StringUtils.nullIfEmpty(StringUtils.clearHtml(text).trim()));
 				break;
 			}
 			case EXPECT_COMMENT:
