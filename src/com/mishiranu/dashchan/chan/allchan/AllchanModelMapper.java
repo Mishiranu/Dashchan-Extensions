b2b02b261df9ca3f51bb0e92dde1576ee76c85b7
Mishiranu <fukurou.mishiranu@gmail.com>
2016-06-29 21:16:09 +0300

    Add user agent icons support

--- src/com/mishiranu/dashchan/chan/allchan/AllchanModelMapper.java
+++ src/com/mishiranu/dashchan/chan/allchan/AllchanModelMapper.java
@@ -2,16 +2,22 @@ package com.mishiranu.dashchan.chan.allchan;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.net.Uri;
+
 import chan.content.ChanLocator;
 import chan.content.model.Attachment;
 import chan.content.model.FileAttachment;
+import chan.content.model.Icon;
 import chan.content.model.Post;
 import chan.content.model.Posts;
 import chan.content.model.Threads;
@@ -20,6 +26,22 @@ import chan.util.StringUtils;
 
 public class AllchanModelMapper
 {
+	private static final Pattern UA_ICON_PATTERN = Pattern.compile("(.*?) / (.*?)");
+	
+	private static final Uri URI_ICON_OS = Uri.parse("chan:///res/raw/raw_os");
+	private static final Uri URI_ICON_ANDROID = Uri.parse("chan:///res/raw/raw_os_android");
+	private static final Uri URI_ICON_APPLE = Uri.parse("chan:///res/raw/raw_os_apple");
+	private static final Uri URI_ICON_LINUX = Uri.parse("chan:///res/raw/raw_os_linux");
+	private static final Uri URI_ICON_WINDOWS = Uri.parse("chan:///res/raw/raw_os_windows");
+	
+	private static final Uri URI_ICON_BROWSER = Uri.parse("chan:///res/raw/raw_browser");
+	private static final Uri URI_ICON_CHROME = Uri.parse("chan:///res/raw/raw_browser_chrome");
+	private static final Uri URI_ICON_EDGE = Uri.parse("chan:///res/raw/raw_browser_edge");
+	private static final Uri URI_ICON_FIREFOX = Uri.parse("chan:///res/raw/raw_browser_firefox");
+	private static final Uri URI_ICON_OPERA = Uri.parse("chan:///res/raw/raw_browser_opera");
+	private static final Uri URI_ICON_SAFARI = Uri.parse("chan:///res/raw/raw_browser_safari");
+	private static final Uri URI_ICON_VIVALDI = Uri.parse("chan:///res/raw/raw_browser_vivaldi");
+	
 	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'", Locale.US);
 	
 	static
@@ -88,6 +110,45 @@ public class AllchanModelMapper
 			post.setComment(text);
 		}
 		post.setCommentMarkup(CommonUtils.optJsonString(jsonObject, "rawText"));
+		ArrayList<Icon> icons = null;
+		String extraData = CommonUtils.optJsonString(jsonObject, "extraData");
+		if (extraData != null)
+		{
+			Matcher matcher = UA_ICON_PATTERN.matcher(extraData);
+			if (matcher.matches())
+			{
+				String browser = matcher.group(1);
+				String os = matcher.group(2);
+				if (!"Other".equals(os))
+				{
+					Uri osIconUri = URI_ICON_OS;
+					if (os.startsWith("Windows")) osIconUri = URI_ICON_WINDOWS;
+					else if (os.startsWith("Linux")) osIconUri = URI_ICON_LINUX;
+					else if (os.startsWith("Ubuntu")) osIconUri = URI_ICON_LINUX;
+					else if (os.startsWith("Mac")) osIconUri = URI_ICON_APPLE;
+					else if (os.startsWith("iOS")) osIconUri = URI_ICON_APPLE;
+					else if (os.startsWith("Android")) osIconUri = URI_ICON_ANDROID;
+					if (icons == null) icons = new ArrayList<>();
+					icons.add(new Icon(locator, osIconUri, os));
+				}
+				if (!"Other 0.0.0".equals(browser))
+				{
+					Uri browserIconUri = URI_ICON_BROWSER;
+					if (browser.startsWith("Chrom")) browserIconUri = URI_ICON_CHROME;
+					else if (browser.startsWith("Firefox")) browserIconUri = URI_ICON_FIREFOX;
+					else if (browser.startsWith("Iceweasel")) browserIconUri = URI_ICON_FIREFOX;
+					else if (browser.startsWith("Edge")) browserIconUri = URI_ICON_EDGE;
+					else if (browser.startsWith("IE")) browserIconUri = URI_ICON_EDGE;
+					else if (browser.startsWith("Opera")) browserIconUri = URI_ICON_OPERA;
+					else if (browser.startsWith("Vivaldi")) browserIconUri = URI_ICON_VIVALDI;
+					else if (browser.startsWith("Safari")) browserIconUri = URI_ICON_SAFARI;
+					else if (browser.startsWith("Mobile Safari")) browserIconUri = URI_ICON_SAFARI;
+					if (icons == null) icons = new ArrayList<>();
+					icons.add(new Icon(locator, browserIconUri, browser));
+				}
+			}
+		}
+		if (icons != null) post.setIcons(icons);
 		JSONArray filesArray = jsonObject.optJSONArray("fileInfos");
 		if (filesArray != null && filesArray.length() > 0)
 		{
