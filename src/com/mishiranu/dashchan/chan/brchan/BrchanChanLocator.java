5ceaf9cd39047cb5e6b34ae578b22670c8fdf969
Mishiranu <fukurou.mishiranu@gmail.com>
2016-09-29 04:54:27 +0300

    Modify regex pattern for "last 50 messages" thread links support

--- src/com/mishiranu/dashchan/chan/brchan/BrchanChanLocator.java
+++ src/com/mishiranu/dashchan/chan/brchan/BrchanChanLocator.java
@@ -10,7 +10,7 @@ import chan.content.ChanLocator;
 public class BrchanChanLocator extends ChanLocator
 {
 	private static final Pattern BOARD_PATH = Pattern.compile("/\\w+(?:/(?:(?:catalog|index|\\d+)\\.html)?)?");
-	private static final Pattern THREAD_PATH = Pattern.compile("/\\w+/res/(\\d+)\\.html");
+	private static final Pattern THREAD_PATH = Pattern.compile("/\\w+/res/(\\d+)(?:\\+50)?\\.html");
 	private static final Pattern ATTACHMENT_PATH = Pattern.compile("/\\w+/src/\\d+(?:-\\d+)?\\.\\w+");
 
 	public BrchanChanLocator()
