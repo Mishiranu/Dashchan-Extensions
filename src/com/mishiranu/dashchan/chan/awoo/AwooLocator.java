60f5fd6805f09912926ce9c0244c1f75693d434c
Mishiranu <fukurou.mishiranu@gmail.com>
2017-09-11 03:27:38 +0300

    Cleanup locator

--- src/com/mishiranu/dashchan/chan/awoo/AwooLocator.java
+++ src/com/mishiranu/dashchan/chan/awoo/AwooLocator.java
@@ -9,7 +9,7 @@ import chan.content.ChanLocator;
 import chan.util.StringUtils;
 
 public class AwooLocator extends ChanLocator {
-	private static final Pattern BOARD_PATH = Pattern.compile("/\\w+(?:/(?:\\d+|catalog)?)?");
+	private static final Pattern BOARD_PATH = Pattern.compile("/\\w+/?");
 	private static final Pattern THREAD_PATH = Pattern.compile("/\\w+/thread/(\\d+)(?:/.*)?");
 
 	public AwooLocator() {
@@ -19,10 +19,6 @@ public class AwooLocator extends ChanLocator {
 
 	@Override
 	public boolean isBoardUri(Uri uri) {
-		return isBoardUriOrSearch(uri) && StringUtils.isEmpty(uri.getFragment());
-	}
-
-	public boolean isBoardUriOrSearch(Uri uri) {
 		return isChanHostOrRelative(uri) && isPathMatches(uri, BOARD_PATH);
 	}
 
@@ -47,34 +43,26 @@ public class AwooLocator extends ChanLocator {
 
 	@Override
 	public String getThreadNumber(Uri uri) {
-		// TODO
 		return getGroupValue(uri.getPath(), THREAD_PATH, 1);
 	}
 
 	@Override
 	public String getPostNumber(Uri uri) {
-		// TODO
-		String fragment = uri.getFragment();
-		if (fragment != null && fragment.startsWith("p")) {
-			fragment = fragment.substring(1);
-		}
-		return fragment;
+		return null;
 	}
 
 	@Override
 	public Uri createBoardUri(String boardName, int pageNumber) {
-		return pageNumber > 0 ? buildPath(boardName, (pageNumber + 1) + ".html") : buildPath(boardName, "");
+		return pageNumber > 0 ? buildQuery(boardName, "page", Integer.toString(pageNumber)) : buildPath(boardName);
 	}
 
 	@Override
 	public Uri createThreadUri(String boardName, String threadNumber) {
-		// probably wrong
 		return buildPath(boardName, "thread", threadNumber);
 	}
 
 	@Override
 	public Uri createPostUri(String boardName, String threadNumber, String postNumber) {
-		// probably wrong
-		return createThreadUri(boardName, threadNumber).buildUpon().fragment("p" + postNumber).build();
+		return createThreadUri(boardName, threadNumber);
 	}
 }
